2688f378bc7e4f7ed697074dd12940e28fe069f6
aatxag <aatxag@mondragon.edu>
2026-07-10 10:27:53 +0200

    Update finetune_contact.py

--- finetune_contact.py
+++ finetune_contact.py
@@ -1,9 +1,8 @@
 # Copyright (c) Sudeep Dasari, 2023
-
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-
+# Modified by Alaitz Atxa, 2026
+#
+# Licensed under the MIT License.
+# See the LICENSE file in the root directory of this repository.
 import os
 import traceback
 
